27cba2ae89036325837d16a47026be9a4a08ac9c
Ivan Ferrant <ivan@ivanferrant.com>
2018-03-20 20:19:04 +0100

    Subir corpus a la API
    Cuando un usuario selecciona un directorio que contiene un corpus, este
    se comprime y se envia a la API mediante una petición POST

--- src/main/com/warcgenerator/gui/actions/upload/UploadCorpusAction.java
+++ src/main/com/warcgenerator/gui/actions/upload/UploadCorpusAction.java
@@ -1,16 +1,19 @@
 package com.warcgenerator.gui.actions.upload;
 
+import com.warcgenerator.core.rest.ServerRequestService;
 import com.warcgenerator.core.util.ZipUtils;
 import com.warcgenerator.gui.view.WarcGeneratorGUI;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
 public class UploadCorpusAction extends AbstractAction implements Observer {
     private WarcGeneratorGUI view;
+    private String corpusZipName;
 
     public UploadCorpusAction(WarcGeneratorGUI view) {
         this.view = view;
@@ -23,14 +26,20 @@ public class UploadCorpusAction extends AbstractAction implements Observer {
 
         int userSelection = fileChooser.showSaveDialog(view.getMainFrame());
         if (userSelection == JFileChooser.APPROVE_OPTION) {
-            File fileToUpload = fileChooser.getSelectedFile();
-            String newCorpusZipName = fileToUpload.getAbsolutePath() + ".zip";
-            new ZipUtils(this, fileToUpload, newCorpusZipName).zipIt();
+            File fileToUpload = fileChooser.getSelectedFile().getAbsoluteFile();
+            corpusZipName = fileToUpload.getName() + ".zip";
+            try {
+                new ZipUtils(this, fileToUpload.getAbsolutePath(), corpusZipName).pack();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 
     @Override
     public void update(Observable observable, Object o) {
-
+        ServerRequestService serverRequestService = new ServerRequestService();
+        serverRequestService.postCorpus(corpusZipName, corpusZipName);
     }
+
 }
