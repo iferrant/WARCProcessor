0a6a005d327eab66d0a45682b382a6849809e6a0
Ivan Ferrant <ivan@ivanferrant.com>
2018-05-15 12:48:49 +0200

    Arregla comprobación de corpus #15
    Un corpus puede ser completamente ham o spam. De ser así, puede darse el
    caso de que la carpeta con 0 elementos exista, por lo que es necesario
    tener este caso a la hora de comprobar si un corpus es correcto para ser
    subido al servidor.

--- src/main/com/warcgenerator/core/helper/CorpusValidatorHelper.java
+++ src/main/com/warcgenerator/core/helper/CorpusValidatorHelper.java
@@ -10,16 +10,28 @@ import java.nio.file.Paths;
 public class CorpusValidatorHelper {
 
     /**
-     * Check if the folder contains a Corpus based on the spam/ham folder names
+     * Check if the folder contains a Corpus based on the spam/ham folder names and warc number
      * @param corpusSummary {@link CorpusSummary} object with the corpus summary
      * @param corpusPath String with the path of the corpus
      * @return True if the spam and ham folder's name of the corpus
      *          summary matches the names of the real folders
      */
     public static boolean isCorpusFolder(CorpusSummary corpusSummary, String corpusPath) {
+        boolean isSpamValid = true;
+        boolean isHamValid = true;
         String spamName = corpusPath + File.separator + corpusSummary.getSpamDir();
         String hamName = corpusPath + File.separator + corpusSummary.getHamDir();
-        return (Files.exists(Paths.get(spamName), LinkOption.NOFOLLOW_LINKS)
-                && Files.exists(Paths.get(hamName), LinkOption.NOFOLLOW_LINKS));
+
+        // If the number of spam warcs is 0, this folder could not exist
+        if (corpusSummary.getNumSpamPages() != 0) {
+           isSpamValid = Files.exists(Paths.get(spamName), LinkOption.NOFOLLOW_LINKS);
+        }
+
+        // If the number of ham warcs is 0, this folder could not exist
+        if (corpusSummary.getNumHamPages() != 0) {
+            isHamValid = Files.exists(Paths.get(hamName), LinkOption.NOFOLLOW_LINKS);
+        }
+
+       return isSpamValid && isHamValid;
     }
 }
